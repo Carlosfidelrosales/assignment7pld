c6aa96af1b8a75cea7800865d0f6adc8cbccdc84
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-27 20:53:22 +0800

    Added some python colors for design, program FINISHED

--- wordsvowelsconsonants.py
+++ wordsvowelsconsonants.py
@@ -1,6 +1,15 @@
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
 def words_vowels_consonants():
-    print("WELCOME TO CARLITO'S WORDS, VOWELS AND CONSONANTS COUNTER!")
-    word_want = input("Please Enter Your Desired Word / Sentences : ")
+    print("\033[1mWELCOME TO \033[31mCARLITO'S WORDS, VOWELS AND CONSONANTS COUNTER\033[0m\033[0m!")
+    word_want = input("\033[92mPlease Enter Your Desired \033[1mWord / Sentence / Phrase\033[0m :\033[0m\n>>> ")
     word_want.lower()
     vowels_count = 0
     vowel = set("aeiouAEIOU")
@@ -14,13 +23,13 @@ def words_vowels_consonants():
             consonants_count = consonants_count + 1
     
     #FOR NUMBER OF WORDS
-    print (f'The number of word(s) the {word_want} have is = {numb}')
+    print (f'The number of \033[96m\033[1mword(s)\033[0m\033[0m the \033[4m{word_want}\033[0m have is = \033[93m\033[1m{numb}\033[0m\033[0m')
 
-    #FOR NUMBER OF VOWELS_count
-    print(f'The total number of vowel(s) are there in {word_want} = {vowels_count}') 
+    #FOR NUMBER OF VOWELS
+    print(f'The total number of \033[96m\033[1mvowel(s)\033[0m\033[0m are there in \033[4m{word_want}\033[0m is = \033[93m\033[1m{vowels_count}\033[0m\033[0m') 
     
     #FOR NUMBER OF CONSONANTS
-    print(f'The total number of consonant(s) are there in {word_want} = {consonants_count}') 
+    print(f'The total number of \033[96m\033[1mconsonant(s)\033[0m\033[0m are there in \033[4m{word_want}\033[0m is = \033[93m\033[1m{consonants_count}\033[93m\033[0m') 
     
     
 words_vowels_consonants()
