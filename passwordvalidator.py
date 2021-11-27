9b63c4eb93632aa3a1f9d2e73aee97f77162c9e8
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-27 20:52:30 +0800

    Added some design, program FINISHED

--- passwordvalidator.py
+++ passwordvalidator.py
@@ -12,25 +12,25 @@ def checker_password(password):
     val = True
       
     if len(password) < 15:
-        print('The password length should be at least 15')
+        print('\033[31mThe password length should be at least \033[1m15 characters\033[0m\033[31m. \033[36mPlease Try Again.\033[0m')
         val = False
     elif not any(type for type in password):
-        print('Password should have at least one capital letter. Please Try Again')
+        print('\033[31mPassword should have at least \033[1mone capital letter\033[0m\033[0m. \033[36mPlease Try Again\033[0m')
         val = False
     elif not any(type.isdigit() for type in password):
-        print('Password should have at least one number. Please Try Again')
+        print('\033[31mPassword should have at \033[1mleast one number\033[0m\033[0m. \033[36mPlease Try Again\033[0m')
         val = False  
     elif not any(type in SpecialSymbols for type in password):
-        print('Password should have at least one special characters !@#$%^&*()_+-. Please Try Again. ')
+        print('\033[31mPassword should have at least \033[1mone of the special characters!@#$%^&*()_+-\033[0m \033[4m\033[0m\033[0m. \033[36mPlease Try Again.\033[0m')
         val = False
     else:
         return val
 
 def enter_passwd():
-    print("WELCOME TO CARLITO'S PASSWORD CHECKER!")
-    password = input('\nEnter your desired password: ')
+    print("\033[33mWELCOME TO \033[1mCARLITO'S PASSWORD CHECKER!\033[33m\033[0m")
+    password = input('\nEnter your \033[4mdesired password\033[0m: ')
     if (checker_password(password)):
-        print("\nCongratulations! Your Password is Valid!")
+        print("\n\033[92mCongratulations! Your Password is \033[1m\033[4mValid\033[0m\033[0m\033[0m!")
     else:
-        print("\nI'm sorry to say that your password is Invalid!")        
+        print("\n\033[92mI'm sorry to say that your password is \033[1m\033[4mInvalid\033[0m\033[0m\033[0m!")        
 enter_passwd()
