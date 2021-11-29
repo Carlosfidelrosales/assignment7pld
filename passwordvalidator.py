c9d242164d223d21f953b406586a5cb0f53119d4
Carlosfidelrosales <fidelcarlos302@gmail.com>
2021-11-29 17:02:58 +0800

    Some parts of the program is revised, PROGRAM FINISHED

--- passwordvalidator.py
+++ passwordvalidator.py
@@ -6,31 +6,39 @@
 #input: P@ssw0rd+P@ssw0rd
 #ouput: Valid
 
-def checker_password(password):
-      
-    SpecialSymbols =['!', '@', '#', '$', '%', '^','&','*','(',')', '_', '+', '-', '/' ]
-    val = True
-      
+import re #Adding re to satisfy the needs of the if, elif, else statements.
+
+print("\033[33mWELCOME TO \033[1mCARLITO'S PASSWORD CHECKER!\033[33m\033[0m")
+
+# Printing the criterias so that the user can have a guide on typing password.
+Criterias = "\033[1m\033[36mYour password must have : \n A. Atleast 15 characters long; \n B. Atleast one capital letter; \n C. Atleast one number; \n D. Atleast one special character.\033[0m\033[0m"
+print(f"{Criterias}") 
+
+    # The program will ask for input from the user
+password = input('\n\033[41mEnter your \033[4mdesired password\033[0m: \033[0m') 
+    
+    # Special Symbols Supported By The Program: ['!', '@', '#', '$', '%', '^','&','*','(',')', '_', '+', '-', '/', '=' ]
+    
+while True:     
     if len(password) < 15:
         print('\033[31mThe password length should be at least \033[1m15 characters\033[0m\033[31m. \033[36mPlease Try Again.\033[0m')
-        val = False
-    elif not any(type for type in password):
+        pswd = 0
+        break
+    elif not re.search("[A-Z]", password):
         print('\033[31mPassword should have at least \033[1mone capital letter\033[0m\033[0m. \033[36mPlease Try Again\033[0m')
-        val = False
-    elif not any(type.isdigit() for type in password):
+        pswd = 0
+        break
+    elif not re.search("[0-9]", password):
         print('\033[31mPassword should have at \033[1mleast one number\033[0m\033[0m. \033[36mPlease Try Again\033[0m')
-        val = False  
-    elif not any(type in SpecialSymbols for type in password):
-        print('\033[31mPassword should have at least \033[1mone of the special characters!@#$%^&*()_+-\033[0m \033[4m\033[0m\033[0m. \033[36mPlease Try Again.\033[0m')
-        val = False
+        pswd = 0
+        break
+    elif not re.search("[!@#$%^&*()_+-/]", password):
+        print('\033[31mPassword should have at least \033[1mone of the special characters ! @ # $ % ^ & * ( ) _ + - / =\033[0m \033[4m\033[0m\033[0m. \033[36mPlease Try Again.\033[0m')
+        pswd = 0
+        break
     else:
-        return val
-
-def enter_passwd():
-    print("\033[33mWELCOME TO \033[1mCARLITO'S PASSWORD CHECKER!\033[33m\033[0m")
-    password = input('\nEnter your \033[4mdesired password\033[0m: ')
-    if (checker_password(password)):
-        print("\n\033[92mCongratulations! Your Password is \033[1m\033[4mValid\033[0m\033[0m\033[0m!")
-    else:
-        print("\n\033[92mI'm sorry to say that your password is \033[1m\033[4mInvalid\033[0m\033[0m\033[0m!")        
-enter_passwd()
+        pswd = 1
+        print("\n\033[92m>>Congratulations! Your Password is \033[1m\033[4mValid\033[0m\033[0m\033[92m!<<\033[0m\033[0m\n")
+        break 
+if pswd == 0:
+    print("\n\033[92m>>I'm sorry to say that your password is \033[1m\033[4mInvalid\033[0m\033[0m\033[0m!<<\033[0m\033[0m\n")
